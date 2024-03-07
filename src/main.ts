c0852165b229b127f1b0f378f5dc55fd4a4592c6
kartikey <98679116+Kartikey-Tiwari@users.noreply.github.com>
2024-03-07 08:34:38 +0530

    Use oneliner createFungible

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,14 @@
 import {
   mplTokenMetadata,
-  createV1,
-  mintV1,
-  TokenStandard,
+  createFungible,
 } from "@metaplex-foundation/mpl-token-metadata";
-import { keypairIdentity } from "@metaplex-foundation/umi";
+import {
+  keypairIdentity,
+  generateSigner,
+  percentAmount,
+} from "@metaplex-foundation/umi";
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
 import { userKeypair } from "./helpers";
-import { generateSigner } from "@metaplex-foundation/umi";
-import {} from "@metaplex-foundation/mpl-token-metadata";
-import { percentAmount } from "@metaplex-foundation/umi";
-import {} from "@metaplex-foundation/mpl-token-metadata";
 
 const umi = createUmi("https://api.devnet.solana.com");
 
@@ -25,8 +23,8 @@ const metadata = {
 };
 
 const mint = generateSigner(umi);
-async function createMetadataDetails() {
-  await createV1(umi, {
+async function createFungibleOneLiner() {
+  createFungible(umi, {
     mint,
     authority: umi.identity,
     name: metadata.name,
@@ -34,16 +32,34 @@ async function createMetadataDetails() {
     uri: metadata.uri,
     sellerFeeBasisPoints: percentAmount(0),
     decimals: 9,
-    tokenStandard: TokenStandard.Fungible,
   }).sendAndConfirm(umi);
 }
-
-async function mintToken() {
-  await mintV1(umi, {
-    mint: mint.publicKey,
-    authority: umi.identity,
-    amount: 10_000,
-    tokenOwner: umi.identity.publicKey,
-    tokenStandard: TokenStandard.Fungible,
-  }).sendAndConfirm(umi);
-}
+createFungibleOneLiner()
+  .then(() =>
+    console.log(
+      `success \ntx: https://explorer.solana.com/address/${mint.publicKey}?cluster=devnet`,
+    ),
+  )
+  .catch((err) => console.error("error minting the tokens", err));
+// async function createMetadataDetails() {
+//   await createV1(umi, {
+//     mint,
+//     authority: umi.identity,
+//     name: metadata.name,
+//     symbol: metadata.symbol,
+//     uri: metadata.uri,
+//     sellerFeeBasisPoints: percentAmount(0),
+//     decimals: 9,
+//     tokenStandard: TokenStandard.Fungible,
+//   }).sendAndConfirm(umi);
+// }
+//
+// async function mintToken() {
+//   await mintV1(umi, {
+//     mint: mint.publicKey,
+//     authority: umi.identity,
+//     amount: 10_000,
+//     tokenOwner: umi.identity.publicKey,
+//     tokenStandard: TokenStandard.Fungible,
+//   }).sendAndConfirm(umi);
+// }
